b39a45f45ec6226e06b8b4c667e2f4db837e8580
Kateryna <katyaukr2014@gmail.com>
2025-06-07 13:55:56 +0300

    add header

---
+++ src/js/mobile-menu.js
@@ -0,0 +1,54 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const menuBtn = document.querySelector('.menu-button');
+    const iconContainer = menuBtn.querySelector('.icon');
+    const modalMenu = document.querySelector('.modal');
+    const menuItem = document.querySelectorAll('.menu-list-item');
+    const header = document.querySelector('.header');
+  
+    let isMenuOpen = false;
+  
+    const burgerIcon = `
+<svg width="36" height="24" viewBox="0 0 36 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M0 24V21H36V24H0ZM0 13.5V10.5H36V13.5H0ZM0 3V0H36V3H0Z" fill="#FFF1E0" />
+</svg>
+    `;
+  
+    const crossIcon = `
+<svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M12.8 38L10 35.2L21.2 24L10 12.8L12.8 10L24 21.2L35.2 10L38 12.8L26.8 24L38 35.2L35.2 38L24 26.8L12.8 38Z" fill="#4B3B2B" />
+</svg>
+    `;
+  
+    menuBtn.addEventListener('click', () => {
+      isMenuOpen = !isMenuOpen;
+      iconContainer.innerHTML = isMenuOpen ? crossIcon : burgerIcon;
+      isMenuOpen ? openModal() : closeModal();
+    });
+  
+    menuItem.forEach(item =>
+      item.addEventListener('click', () => {
+        closeModal();
+        iconContainer.innerHTML = burgerIcon;
+        isMenuOpen = false;
+      })
+    );
+  
+    window.addEventListener('scroll', blurHeader);
+  
+    function openModal() {
+      modalMenu.style.display = 'block';
+    }
+  
+    function closeModal() {
+      modalMenu.style.display = 'none';
+    }
+  
+    function blurHeader() {
+      if (!header) return;
+      if (window.scrollY > 60) {
+        header.classList.add('header-scroll');
+      } else {
+        header.classList.remove('header-scroll');
+      }
+    }
+  });
